a3d945f8e914c8d58088d0ec50fa2d3160ccd806
DZWG <paule22@protonmail.com>
2023-03-12 19:57:55 +0100

    updated psql2

---
+++ substream-sa/psql/schema.sql
@@ -0,0 +1,20 @@
+create table block_meta
+(
+    id          text not null
+        constraint block_meta_pk primary key,
+    version     integer,
+    at          text,
+    number      text,
+    hash        text,
+    parent_hash text,
+    timestamp   text
+);
+
+create table cursors
+(
+    id        text not null
+        constraint cursor_pk primary key,
+    cursor    text,
+    block_num bigint,
+    block_id  text
+);
