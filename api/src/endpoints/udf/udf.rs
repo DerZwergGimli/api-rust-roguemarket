df48b2a5d003b23901d3174d799025faf9c33952
DZWG <paule22@protonmail.com>
2023-03-13 22:12:41 +0100

    added false to to from

--- api/src/endpoints/udf/udf.rs
+++ api/src/endpoints/udf/udf.rs
@@ -429,7 +429,7 @@ pub async fn get_history(
     if query.countback.unwrap_or_default() > 0 {
         cursor_db = trades
             .filter(symbol.like(query.symbol.clone())
-                .and(timestamp.gt(query.to.unwrap_or_default() as i64 - query.countback.unwrap_or_default() as i64))
+                .and(timestamp.gt(query.from.unwrap_or_default() as i64 - query.countback.unwrap_or_default() as i64))
                 .and(timestamp.lt(query.to.unwrap_or_default() as i64)))
             .load::<Trade>(&mut db)
             .expect("Error loading cursors");
